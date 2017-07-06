ac29a4e8787bcc234db9bdd9cfc08099a8b9482f
java-crypto <bm98@arcor.de>
2017-07-06 16:41:50 +0200

    Add files via upload

---
+++ A01_HalloWelt.java
@@ -0,0 +1,34 @@
+package javaCrypto;
+
+/*
+* Programm/Program A01_HalloWelt.java
+* Kurzbeschreibung/Short description:
+* Erstes Java-Programm / First Java program 
+* Herkunft/Origin: http://javacrypto.bplaced.net/
+* Sourcecode: https://github.com/java-crypto/A-Allgemeine-Programme/
+* Programmierer/Programmer: Michael Fehr
+*
+* getestet mit/checked with Java Runtime Environment 8 Update 131
+* letzter Test/last check: 06.07.2017/July 6th. 2017 
+*
+* Copyright/Copyright: frei verwendbares Programm/Public Domain
+* Lizenzmodell/Licence model: CC0 (Public Domain wo verfügbar/where available)
+* Lizenzhinweise/Licence links:
+* https://de.wikipedia.org/wiki/Creative_Commons#CC_Zero
+* https://en.wikipedia.org/wiki/Creative_Commons_license
+* 
+* Sicherheitshinweis/Security notice
+* Die Programmroutinen dienen nur der Darstellung und haben keinen Anspruch auf eine korrekte Funktion, 
+* insbesondere mit Blick auf die Sicherheit ! 
+* Prüfen Sie die Sicherheit bevor die Programm in der echten Welt eingesetzt werden.
+* The program routines just show the function but please be aware of the security part - 
+* check yourself before using in the real world !
+*/
+
+public class A01_HalloWelt {
+
+	public static void main(String[] args) {
+		System.out.println("A01 Hallo Welt");
+		System.out.println("Hallo Welt");
+	}
+}
