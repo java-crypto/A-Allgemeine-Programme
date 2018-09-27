b5fcfd8126c1837142ab3ae476b535f881a167c6
Michael Fehr <javacrypto@gmx.de>
2018-09-27 11:56:40 +0200

    Add files via upload

---
+++ A02_JavaSystemVariables.java
@@ -0,0 +1,29 @@
+package net.bplaced.javacrypto.general;
+
+/*
+* Herkunft/Origin: http://javacrypto.bplaced.net/
+* Programmierer/Programmer: Michael Fehr
+* Copyright/Copyright: frei verwendbares Programm (Public Domain)
+* Copyright: This is free and unencumbered software released into the public domain.
+* Lizenttext/Licence: <http://unlicense.org>
+* getestet mit/tested with: Java Runtime Environment 8 Update 181 x64
+* Datum/Date (dd.mm.jjjj): 25.09.2018
+* Funktion: gibt die Java-Systemvariablen aus
+* Function: output of the Java system-variables
+*
+* Sicherheitshinweis/Security notice
+* Die Programmroutinen dienen nur der Darstellung und haben keinen Anspruch auf eine  
+* korrekte Funktion, insbesondere mit Blick auf die Sicherheit ! 
+* Prüfen Sie die Sicherheit bevor das Programm in der echten Welt eingesetzt wird.
+* The program routines just show the function but please be aware of the security part - 
+* check yourself before using in the real world !
+*/
+
+public class A02_JavaSystemVariables {
+
+	public static void main(String[] args) {
+		System.out.println("Java-Version:" + System.getProperty("java.version"));
+		java.util.Properties properties = System.getProperties();
+		properties.list(System.out);
+	}
+}
